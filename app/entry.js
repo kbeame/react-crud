8279f5a30704b1856b3125ec52b628c91c7125af
Katherine Beame <katherine.beame@gmail.com>
2016-05-15 23:24:04 -0700

    cleaned up dead code

--- app/entry.js
+++ app/entry.js
@@ -1,27 +1,7 @@
 const React = require('react');
 const ReactDom = require('react-dom');
-// const fetch = require('whatwg-fetch');
 const $ = require('jquery');
 
-var pets = [
-    {
-        "__v": 0,
-        "_id": "5738f17b267b824d25c6db3d",
-        "favoriteActivity": "cuddles",
-        "name": "Ellie",
-        "nickName": "toot"
-    },
-    {
-        "__v": 0,
-        "_id": "5738f17b267b824d25c6db3d",
-        "favoriteActivity": "cuddles",
-        "name": "Moose",
-        "nickName": "toot"
-    }
-]
-// var LoadPets = React.createClass({
-//   getInitialState
-// })
 
 var List = React.createClass({
   render: function () {
@@ -72,20 +52,3 @@ var App = React.createClass({
 
 
 ReactDom.render(<App></App>, document.getElementById('app'));
-
-
-
-// this.pet = [];
-//   this.getAll = () => {
-//     $http.get(url + '/api/pet')
-//     .then((res) => {
-//       this.pet = res.data;
-//     }, handleErrors.bind(this));
-//   };
-//   this.createPet = () => {
-//     $http.post(url + '/api/pet', this.newPet)
-//       .then((res) => {
-//         this.pet.push(res.data);
-//         this.newPet = null;
-//       }, handleErrors.bind(this));
-//   };
